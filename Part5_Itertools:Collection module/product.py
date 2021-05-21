f6a0535191066c061a76d5c9c6adfdaa947d5975
Geunwoo Park <qkr0677@gmail.com>
2021-05-21 21:57:24 +0900

    Create product.py

---
+++ Part5_Itertools:Collection module/product.py
@@ -0,0 +1,9 @@
+#곱집합 구하기
+from itertools import product
+
+import itertools
+
+iterable1 = 'ABCD'
+iterable2 = 'xy'
+iterable3 = '1234'
+print(list(itertools.product(iterable1, iterable2, iterable3)))
