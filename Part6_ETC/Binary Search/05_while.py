9a252756ce553431e7c9aada82d52f596dd4a48a
Geunwoo Park <qkr0677@gmail.com>
2021-05-22 23:52:07 +0900

    Create 05_while.py

---
+++ Part6_ETC/Binary Search/05_while.py
@@ -0,0 +1,31 @@
+arr = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11]
+
+# list를 넣고 숫자를 찾는다.
+
+
+def binarySearch(arr, targetNum):
+    # 유틸 변수(도우미 변수)
+    start = 0
+    end = len(arr) - 1
+    while (start <= end):
+        # 중간 인덱스
+        midIndex = len(arr) // 2  # 5
+
+        # 중간 인덱스 값 저장
+        indexValue = arr[midIndex]
+
+        # 중간 인덱스 값이 찾는 대상이면 중간 인덱스 리턴
+        if indexValue == targetNum:
+            return midIndex
+        elif indexValue < targetNum:
+            start = midIndex + 1
+        elif indexValue > targetNum:
+            end = midIndex - 1
+
+    print("start :", start, "end :", end)
+    return -1
+
+
+# 1------5--8---11
+# 5-----8--11
+print(binarySearch(arr, 8))
