d23446c8fca175519a91aa4cdb5faa2520051c9c
Geunwoo Park <qkr0677@gmail.com>
2021-05-22 23:51:50 +0900

    Create 01_start.py

---
+++ Part6_ETC/Binary Search/01_start.py
@@ -0,0 +1,13 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+# list를 넣고 숫자를 찾는다.
+
+
+def binarySearch(arr, targetNum):
+    # 중간 인덱스 값
+    midIndex = len(arr) // 2  # 5
+    print(midIndex)
+    return -1
+
+
+print(binarySearch(arr, 8))
