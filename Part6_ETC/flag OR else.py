fd956b52a2f465eaf91ad785956f632cda3492c1
Geunwoo Park <qkr0677@gmail.com>
2021-05-23 23:24:54 +0900

    Update flag OR else.py

--- Part6_ETC/flag OR else.py
+++ Part6_ETC/flag OR else.py
@@ -1,3 +1,6 @@
+# 숫자를 차례로 곱해 나온 수가 제곱수1가 되면 found를 출력하고
+# 모든 수를 곱해도 제곱수가 나오지 않았다면 not found를 출력
+
 from math import sqrt
 
 multiplied = 1
@@ -6,7 +9,8 @@ for i in range(5):
 
     multiplied *= a
 
-if sqrt(multiplied)**2 == multiplied:
-    print("found")
+    if multiplied == int(sqrt(multiplied))**2:
+        print("found")
+        break
 else:
     print("not found")
