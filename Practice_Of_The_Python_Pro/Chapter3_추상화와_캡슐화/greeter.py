c9fca106f5abcfd511788b7532da17f80249c1fa
Gnu-Kenny <qkr0677@gmail.com>
2021-12-30 15:47:59 +0900

    refact: day, part_of_day 메서드 Greeter 클래스와 분리
    - 클래스 명과 메서드의 기능이 독립적이라 판단 -> 관심사 분리

--- Practice_Of_The_Python_Pro/Chapter3_추상화와_캡슐화/greeter.py
+++ Practice_Of_The_Python_Pro/Chapter3_추상화와_캡슐화/greeter.py
@@ -3,32 +3,33 @@ from time import ctime
 from datetime import datetime
 
 
+def day():
+    """
+    현재 요일 반환
+    """
+    return datetime.now().strftime("%A")
+
+
+def part_of_day():
+    """
+    현재 시각에 해당하는 값을 반환한다.
+    """
+
+    current_hour = datetime.now().hour
+
+    if current_hour < 12:
+        part_of_day = "morning"
+    elif current_hour < 17:
+        part_of_day = "afternoon"
+    else:
+        part_of_day = "evening"
+
+    return part_of_day
+
+
 class Greeter:
     def __init__(self, name):
         self.name = name
-
-    @staticmethod
-    def _day():
-        """
-        현재 요일 반환
-        """
-        return datetime.now().strftime("%A")
-
-    def _part_of_day(self):
-        """
-        현재 시각에 해당하는 값을 반환한다.
-        """
-
-        current_hour = datetime.now().hour
-
-        if current_hour < 12:
-            part_of_day = "morning"
-        elif current_hour < 17:
-            part_of_day = "afternoon"
-        else:
-            part_of_day = "evening"
-
-        return part_of_day
 
     def greet(self, store):
         """
@@ -37,7 +38,7 @@ class Greeter:
         """
 
         message = f"""Hi, my name is {self.name}, and welcome to {store}!\
-        \nHow's your {self._day()}, {self._part_of_day()} going?\
+        \nHow's your {day()}, {part_of_day()} going?\
         \nHere's a coupon for 20% off!"""
         return print(message)
 
