9758fcfc446095bc6214ca69bd011da7679eff98
Gnu-Kenny <qkr0677@gmail.com>
2022-02-20 14:17:12 +0900

    fix: 열의 앞쪽에 전투력이 같은 병사가 있다면 열에서 제외하도록 수정

--- Algorithm/이코테/DP/병사 배치하기.py
+++ Algorithm/이코테/DP/병사 배치하기.py
@@ -11,7 +11,7 @@ def main():
     for i in range(n):
 
         for j in range(i):
-            if arr[j] >= arr[i]:
+            if arr[j] > arr[i]:
                 dp[i] = max(dp[i], dp[j] + 1)
     
     print(n-max(dp))
