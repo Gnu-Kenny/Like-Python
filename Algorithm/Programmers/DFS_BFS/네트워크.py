f2d478e57c045d5c5ff5cbd8cba512a3aa95c6ba
Gnu-Kenny <qkr0677@gmail.com>
2022-02-28 13:59:22 +0900

    [220228] PROG_BFS_DFS_네트워크

---
+++ Algorithm/Programmers/DFS_BFS/네트워크.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+visit = []
+alist = []
+def bfs(network:list):
+    global visit, alist
+    for computer in network:
+        if visit[computer]:
+            continue
+        visit[computer] = 1
+        bfs(alist[computer])
+def solution(n, computers):
+    global visit, alist
+    visit = [0 for _ in range(n)]
+    alist = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j and computers[i][j] == 1:
+                alist[i].append(j)
+    answer = 0
+    for i in range(n):
+        if visit[i] == 0:
+            answer += 1
+            bfs(alist[i])
+    
+    return answer
+
+def main():
+    n = 3
+    computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]	
+    print(solution(n, computers))
+
+if __name__ == "__main__":
+    main()
