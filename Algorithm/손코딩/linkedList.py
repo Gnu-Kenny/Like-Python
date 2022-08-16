84746f2376503288625f6101be44911545fc7cb5
GeunWooPark <qkr0677@gmail.com>
2022-08-16 22:42:37 +0900

    단일 링크드 리스트 구현 - 삽입 삭제 조회 기능 추가

--- Algorithm/손코딩/linkedList.py
+++ Algorithm/손코딩/linkedList.py
@@ -4,10 +4,38 @@ class Node():
         self.next = None
 
 
-head_node = Node(1)
-head_node.next = Node(2)
-head_node.next.next = Node(3)
-head_node.next.next.next = Node(4)
+class SLinkedList():
+    def __init__(self):
+        self.head = None
+
+    def addAtHead(self, value):  # O(1)
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
+    def addBack(self, value):  # O(n)
+        node = Node(value)
+        crnt_node = self.head
+        while crnt_node.next:
+            crnt_node = crnt_node.next
+        crnt_node.next = node
+
+    def findNode(self, value):  # O(n)
+        crnt_node = self.head
+        while crnt_node is not None:
+            if crnt_node.value == value:
+                return crnt_node
+            crnt_node = crnt_node.next
+        raise RuntimeError('Node not found')
+
+    def addAfter(self, node: Node, value):  # O(1)
+        new_node = Node(value)
+        new_node.next = node.next
+        node.next = new_node
+
+    def deleteAfter(self, prev_node: Node):  # O(1)
+        if prev_node.next is not None:
+            prev_node.next = prev_node.next.next
 
 
 def printNodes(node: Node):
@@ -18,4 +46,20 @@ def printNodes(node: Node):
         crnt_node = crnt_node.next
 
 
-printNodes(head_node)
+slist = SLinkedList()
+slist.addAtHead(1)
+slist.addAtHead(2)
+slist.addBack(3)
+
+node1 = slist.findNode(1)
+slist.addAfter(node1, 4)
+slist.deleteAfter(node1)
+
+printNodes(slist.head)
+# head_node = Node(1)
+# head_node.next = Node(2)
+# head_node.next.next = Node(3)
+# head_node.next.next.next = Node(4)
+
+
+# printNodes(head_node)
