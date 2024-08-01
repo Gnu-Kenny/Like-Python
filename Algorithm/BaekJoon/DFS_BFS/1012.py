9e15f535d4480d07b09e3130f7e7a8db73f95ddb
Gnu-Kenny <qkr0677@gmail.com>
2024-08-01 20:02:31 +0900

    깊이우선탐색

--- Algorithm/BaekJoon/DFS_BFS/1012.py
+++ Algorithm/BaekJoon/DFS_BFS/1012.py
@@ -1,73 +1,70 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
+limit_number = 15000
+sys.setrecursionlimit(limit_number)
 
-class Node:
-    def __init__(self, y, x):
-        self.y, self.x = y, x
+t = int(input())
+m, n, k = 0,0,0
+board = []
+visited = []
+
+dy = [0,0,-1,1]
+dx = [-1,1,0,0]
 
 
-w = 0
-h = 0
-v_num = 0
-ground = []
-visit = []
-
-
-def bfs(node: Node):
-    global w, h, v_num
-    global ground
-    global visit
-
-    dy = [-1, 1, 0, 0]  # 위, 아래, 좌, 우
-    dx = [0, 0, -1, 1]
-
-    q = deque()
-
-    q.append(node)
-    while q:
-        now = q.popleft()
-
-        for i in range(4):
-            ny = now.y + dy[i]
-            nx = now.x + dx[i]
-
-            if ny < 0 or nx < 0 or ny >= h or nx >= w:
-                continue
-            if visit[ny][nx]:
-                continue
-            if ground[ny][nx] == 0:
-                continue
-            visit[ny][nx] = 1
-            q.append(Node(ny, nx))
+class Node():
+    def __init__(self,y,x):
+        self.y = y
+        self.x = x
 
 
 def main():
-    global w, h, v_num
-    global ground
-    global visit
-
-    t = int(input())
-
+    global t, m, n, k, board, visited
+    
     for _ in range(t):
-        answer = 0
-        ground = []
-        visit = []
-        w, h, v_num = list(map(int, input().split()))
-        ground = [[0 for _ in range(w)] for _ in range(h)]
-        visit = [[0 for _ in range(w)] for _ in range(h)]
-        for _ in range(v_num):
-            x, y = list(map(int, input().split()))  # w, h 순으로 입력
-            ground[y][x] = 1
-        for y in range(h):
-            for x in range(w):
-                if visit[y][x] == 0 and ground[y][x] == 1:
-                    answer += 1
-                    bfs(Node(y, x))
+        # init
+        m, n, k = map(int, input().split())
+        board = [[0 for _ in range(m)]for _ in range(n)]
+        visited = [[0 for _ in range(m)]for _ in range(n)]
+        
+        for _ in range(k):
+            x, y = map(int, input().split())
 
-        print(answer)
+            board[y][x] = 1
+        
+        # 탐색 구간 선정
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == 1 and visited[i][j] == 0:
+                    
+                    dfs(Node(i,j))
+                    
+                    ans += 1
+        
+        print(ans)
 
 
-if __name__ == "__main__":
+def dfs(now: Node):
+    global m, n, board, visited, dy, dx
+    
+    visited[now.y][now.x] = 1
+    
+    for i in range(4):
+        ny = now.y + dy[i]
+        nx = now.x + dx[i]
+        
+        if ny < 0 or nx < 0 or ny >= n or nx >= m:
+            continue
+        
+        if board[ny][nx] == 0:
+            continue
+        
+        if visited[ny][nx] == 1:
+            continue
+        
+        dfs(Node(ny, nx))
+
+if __name__ == '__main__':
     main()
